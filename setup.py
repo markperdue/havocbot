7731811a1e26565bde184c1016fbf68db0a8b262
Mark Perdue <markaperdue@gmail.com>
2016-03-04 16:03:56 -0800

    0.2.0 release - backwards breaking changes. check examples/main.py for new setup

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='havocbot',
-    version='0.1.2',
+    version='0.2.0',
     description='An extensible chat bot that works across any number of chat programs',
     long_description=long_description,
     url='https://github.com/markperdue/havocbot',
